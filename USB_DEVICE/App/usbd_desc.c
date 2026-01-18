dd0e508543af6d0ede00cc32b333de961a9c9f3a
Chanathip <bipoeman12@gmail.com>
2026-01-18 15:54:31 +0700

    edit vid pid and usb string stuff

--- USB_DEVICE/App/usbd_desc.c
+++ USB_DEVICE/App/usbd_desc.c
@@ -62,13 +62,13 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     0xF055
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
-#define USBD_CONFIGURATION_STRING_FS     "CDC Config"
-#define USBD_INTERFACE_STRING_FS     "CDC Interface"
+#define USBD_MANUFACTURER_STRING     "Openfruit Aerospace"
+#define USBD_PID_FS     0x0BC1
+#define USBD_PRODUCT_STRING_FS     "OBC VCP"
+#define USBD_CONFIGURATION_STRING_FS     "OBC CDC Config"
+#define USBD_INTERFACE_STRING_FS     "OBC CDC Interface"
 
 #define USB_SIZ_BOS_DESC            0x0C
 
