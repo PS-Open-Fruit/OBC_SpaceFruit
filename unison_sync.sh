c819f9bd5639455692ccd5c203815a5744ad0699
bipoe <bipoeman12@gmail.com>
2026-04-02 13:50:18 +0700

    make unison less agressive

--- unison_sync.sh
+++ unison_sync.sh
@@ -1,5 +1,5 @@
 unison ./eps_and_payload_emulator/kiss_file_transfer/ ssh://bipoe@100.1.1.1//home//bipoe//payload_emulator// \
-    -prefer ./eps_and_payload_emulator/kiss_file_transfer/ \
     -repeat 2 \
     -batch \
     -ignore 'Name .venv'
+    # -prefer ./eps_and_payload_emulator/kiss_file_transfer/ \
